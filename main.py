d4578c180443e45688b2a721f3e75e61bf5a3700
Natty <Nathnaelyirga@gmial.com>
2023-02-14 16:24:50 +0300

    ssh try

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ print('Starting a bot....')
 async def start_commmand(update, context):
     await update.message.reply_text('Hello! Welcome To Store!')
 
+#listen for chat-requiests
+
+#adding him to waiting pool or link him up with someone waiting
+
+# listen for incoming messgaes for his date
+
+# pass his message along
+
 if __name__ == '__main__':
     application = Application.builder().token("6067047983:AAGR7HIiXAKXQTzxztLxLc1oSobI4ESrlJ8").build()
 
